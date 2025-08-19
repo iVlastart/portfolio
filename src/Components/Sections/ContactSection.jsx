ada777535152c8032f2423abcdcad47783b5f2b5
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-08-19 19:27:04 +0200

    Decided to remove the contact form

--- src/Components/Sections/ContactSection.jsx
+++ src/Components/Sections/ContactSection.jsx
@@ -2,7 +2,6 @@ import { Title } from "@/Components/partials/Title";
 import { Mail, MapPin, Phone } from "lucide-react";
 import { ContactInfo } from "@/Components/partials/ContactInfo";
 import { ContactSocials } from "@/Components/partials/ContactSocials";
-import { ContactForm } from "@/Components/partials/ContactForm";
 
 export const ContactSection = ()=>{
     return(
@@ -15,9 +14,9 @@ export const ContactSection = ()=>{
                     I'm always open to discussing new opportunities.
                 </p>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+                <div className="flex items-center justify-center">
                     <div className="space-y-8">
-                        <h3 className="text-2xl font-semibold mb-6">
+                        <h3 className="text-2xl md:text-3xl font-semibold mb-6">
                             Contact Information
                         </h3>
 
@@ -25,14 +24,6 @@ export const ContactSection = ()=>{
                         <ContactInfo/>
 
                         <ContactSocials/>
-                    </div>
-
-                    <div className="bg-card p-8 rounded-lg shadow-xs">
-                        <h3 className="text-2xl font-semibold mb-6">
-                           Send a Message 
-                        </h3>
-
-                        <ContactForm/>
                     </div>
                 </div>
             </div>
