230cf801ee5535e08d1074373873821cc07a3c94
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-08-22 17:01:58 +0200

    Updated LibreIo project

--- src/Components/Sections/ProjectsSection.jsx
+++ src/Components/Sections/ProjectsSection.jsx
@@ -4,8 +4,8 @@ import { ArrowRight, ExternalLink, GithubIcon } from "lucide-react";
 const projects = [
     {
         id: 1,
-        title: 'LibreIo (Coming soon)',
-        description: 'A web app where you can upload and share your videos with others',
+        title: 'LibreIo',
+        description: 'A web app where you can upload and share your videos with others. App is not deployed yet.',
         img: '/projects/LibreIo.webp',
         tags: ['Laravel', 'PHP', 'HTML', 'Tailwind CSS', 'JavaScript'],
         github: 'https://github.com/iVlastart/LibreIo',
